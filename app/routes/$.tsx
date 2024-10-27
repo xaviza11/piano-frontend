35510c3bef921e5babcbe291fcf6deb5e23d329d
tryconnection <xaviza11@gmail.com>
2024-10-27 12:21:56 +0100

    refactor MetaFunctions

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -4,10 +4,27 @@ import NotFound from "~/components/NotFound";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "404 - Página no encontrada" },
-    { name: "description", content: "Página no encontrada" },
+    //{ title: "New Remix App" },  //warn change it whit the language
+    //{ name: "description", content: "Welcome to Remix!" },  //warn change it whit the language
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { charSet: "utf-8" },
+    { name: "author", content: "Mateo Javier Zamora Lorente" },
+    //{ name: "keywords", content: "music, piano..." }, //warn change it whit the language
+
+    //{property: "og:title", content: "Tittle" },  //warn change it whit the language
+    //{ property: "og:description", content: "Description" },  //warn change it whit the language
+    { property: "og:image", content: "Image" },
+    //{ property: "og:url", content: "URL of the page" },  //warn change it whit the language
+
+    { name: "twitter:card", content: "summary_large_image" }, 
+    //{ name: "twitter:title", content: "Tittle" },  //warn change it whit the language
+    //{ name: "twitter:description", content: "Description" },  //warn change it whit the language
+    { name: "twitter:image", content: "URL of image" },
+
+    { name: "robots", content: "noindex, nofollow" }, 
   ];
 };
+
 
 export default function NotFoundPage() {
   return <NotFound />;
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -3,10 +3,27 @@ import Register from "~/components/Register";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    //{ title: "New Remix App" },  //warn change it whit the language
+    //{ name: "description", content: "Welcome to Remix!" },  //warn change it whit the language
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { charSet: "utf-8" },
+    { name: "author", content: "Mateo Javier Zamora Lorente" },
+    //{ name: "keywords", content: "music, piano..." }, //warn change it whit the language
+
+    //{property: "og:title", content: "Tittle" },  //warn change it whit the language
+    //{ property: "og:description", content: "Description" },  //warn change it whit the language
+    { property: "og:image", content: "Image" },
+    //{ property: "og:url", content: "URL of the page" },  //warn change it whit the language
+
+    { name: "twitter:card", content: "summary_large_image" }, 
+    //{ name: "twitter:title", content: "Tittle" },  //warn change it whit the language
+    //{ name: "twitter:description", content: "Description" },  //warn change it whit the language
+    { name: "twitter:image", content: "URL of image" },
+
+    { name: "robots", content: "noindex, nofollow" }, 
   ];
 };
+
 
 export default function Index() {
   return (

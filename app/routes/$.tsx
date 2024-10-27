893307d9442b71eda4b3442c9ca276e527020f64
tryconnection <xaviza11@gmail.com>
2024-10-27 12:46:52 +0100

    delete notFound componentent, display it inside page

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,6 +1,5 @@
 // app/routes/$*.tsx
 import { MetaFunction } from "@remix-run/node";
-import NotFound from "~/components/NotFound";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,23 +8,18 @@ export const meta: MetaFunction = () => {
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     { charSet: "utf-8" },
     { name: "author", content: "Mateo Javier Zamora Lorente" },
-    //{ name: "keywords", content: "music, piano..." }, //warn change it whit the language
-
-    //{property: "og:title", content: "Tittle" },  //warn change it whit the language
-    //{ property: "og:description", content: "Description" },  //warn change it whit the language
-    { property: "og:image", content: "Image" },
-    //{ property: "og:url", content: "URL of the page" },  //warn change it whit the language
-
-    { name: "twitter:card", content: "summary_large_image" }, 
-    //{ name: "twitter:title", content: "Tittle" },  //warn change it whit the language
-    //{ name: "twitter:description", content: "Description" },  //warn change it whit the language
-    { name: "twitter:image", content: "URL of image" },
-
-    { name: "robots", content: "noindex, nofollow" }, 
+    { name: "robots", content: "noindex, nofollow" },
   ];
 };
 
 
 export default function NotFoundPage() {
-  return <NotFound />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <h2 className="text-2xl font-bold text-black">404</h2>
+      <h2 className="text-xl mt-4 text-black">Page Not Found</h2>
+      <p className="mt-2 text-gray-800">Sorry, the page you are looking for does not exist.</p>
+      <a href="/" className="mt-4 text-blue-500 hover:underline">Go back to the homepage</a>
+    </div>
+  );
 }
